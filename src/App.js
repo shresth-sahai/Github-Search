4b0d65487ea4ba859e01b6634334eafa4c716677
TEJENDER DUBEY <tejender.dubey@indiamart.com>
2023-10-18 18:36:16 +0530

    added base code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/pages/Home/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg">
+      <Home />
     </div>
   );
 }
 
 export default App;
+/*
+onchange --->  +  debounce 
+remove button search 
+pagination functionality
+cache results --> keyword --> dictionary 
+*/
